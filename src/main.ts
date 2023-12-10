261f1fa99ede6aac22f21a5acd1d96deb8221f98
Chrysovalantis <chrysovalantis.christodoulou@suite5.eu>
2023-12-10 12:05:49 +0200

    initial setup

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Coding Challenge')
+    .setDescription('API for the coding challenge')
+    .setVersion('1.0')
+    .addTag('bdswiss')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
